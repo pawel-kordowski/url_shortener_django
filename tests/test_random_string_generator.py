197686521015876f85df691ed9e47b38bf87b9a6
Paweł Kordowski <pawel.kordowski.codeline@gmail.com>
2022-06-09 01:15:27 +0200

    Use django get_random_string instead of custom implementation

--- tests/test_random_string_generator.py
+++ tests/test_random_string_generator.py
@@ -3,20 +3,6 @@ import pytest
 from url_shortener_app.random_string_generator import RandomStringGenerator
 
 
-@pytest.mark.parametrize("length", (2, 3))
-def test_get_random_string_returns_string_of_a_given_length(length):
-    random_string = RandomStringGenerator.get_random_string(length=length)
-
-    assert type(random_string) is str
-    assert len(random_string) == length
-
-
-def test_get_random_string_returns_different_values():
-    assert RandomStringGenerator.get_random_string(
-        length=10
-    ) != RandomStringGenerator.get_random_string(length=10)
-
-
 @pytest.mark.parametrize("length,count", ((2, 3), (3, 2)))
 def test_get_random_strings_returns_set_of_strings(length, count):
     random_strings = RandomStringGenerator.get_random_strings(
--- url_shortener_app/random_string_generator.py
+++ url_shortener_app/random_string_generator.py
@@ -1,14 +1,7 @@
-import random
-import string
+from django.utils.crypto import get_random_string
 
 
 class RandomStringGenerator:
-    chars = string.ascii_letters + string.digits
-
-    @classmethod
-    def get_random_string(cls, length: int) -> str:
-        return "".join(random.choices(cls.chars, k=length))
-
     @classmethod
     def get_random_strings(cls, length: int, count: int) -> set[str]:
-        return {cls.get_random_string(length=length) for _ in range(count)}
+        return {get_random_string(length=length) for _ in range(count)}
